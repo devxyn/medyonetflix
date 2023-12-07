f45804c4a8c90b380f100255f37262e1856a7c20
devxyn <johnlawrencehermo@gmail.com>
2023-12-07 23:53:06 +0800

    feat: moved the loader from featured to main

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,14 +1,18 @@
+import { useLoaderData } from "react-router-dom";
 import Featured from "../components/Featured";
 import Row from "../components/Row";
 import requests from "./../utils/request";
 
 const Main = () => {
+  const data = useLoaderData();
   const imgUrl = "https://www.themoviedb.org/t/p/";
+
+  const movie = data[Math.floor(Math.random() * data.length)];
 
   return (
     <div>
       {/* Featured Movie on the top*/}
-      <Featured imgUrl={imgUrl} />
+      <Featured imgUrl={imgUrl} movie={movie} height={"h-[550px]"} />
       <Row rowId="1" title="Popular" fetchUrl={requests.requestPopular} imgUrl={imgUrl} />
       <Row rowId="2" title="Top Rated" fetchUrl={requests.requestTopRated} imgUrl={imgUrl} />
       <Row rowId="3" title="Up Coming" fetchUrl={requests.requestUpcoming} imgUrl={imgUrl} />

f76b947705e5c0037080ecc56abcbd2d18802613
devxyn <johnlawrencehermo@gmail.com>
2023-12-07 12:54:23 +0800

    fix: responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import { IoMenu } from "react-icons/io5";
 import { IoClose } from "react-icons/io5";
+import { IoMenu } from "react-icons/io5";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
@@ -11,30 +11,23 @@ const Navbar = () => {
 
   return (
     <div
-      className={`flex flex-col md:flex-row justify-center items-center md:justify-between p-5 w-full absolute z-[99] ${
-        open && "bg-black/80 duration-500 ease-in"
-      }  md:bg-transparent`}>
+      className={`flex flex-col md:flex-row justify-center items-center md:justify-between p-4 w-full absolute z-[99] ${
+        open ? "bg-black/80 transition-all ease-in duration-200" : "transition-all ease-out duration-200"
+      } md:bg-transparent`}>
       <h1 className="text-red-600 text-4xl font-bold">MEDYO NETFLIX</h1>
-      <div onClick={toggleOpen} className="absolute right-4 top-7 md:hidden">
-        {open ? (
-          <IoClose className="text-white cursor-pointer " size={24} />
-        ) : (
-          <IoMenu className="text-white cursor-pointer " size={24} />
-        )}
+      <div className="absolute right-4 top-6 text-white md:hidden" onClick={toggleOpen}>
+        {open ? <IoClose size={24} /> : <IoMenu size={24} />}
       </div>
-      <div
-        className={`flex flex-col justify-center bg-black/80 md:bg-transparent w-full h-80 gap-2 absolute md:inline-block transition-all duration-500 ease-in ${
-          open ? "top-20" : "top-[-700px]"
-        } `}>
+      <div className={`${open ? "flex " : "hidden"} flex-col transition-all ease-in duration-500 md:inline-block`}>
         <input
-          className="hidden md:block rounded py-2 px-4 mt-4 md:mt-0 md:mr-4 bg-transparent border-2 border-black/40 text-white"
+          className="rounded py-2 px-4 mt-4 mb-5 md:mt-0 md:mr-4 bg-transparent border-2 border-black/40 text-white"
           type="text"
           placeholder="Search"
           id="search"
         />
-        <div className="flex flex-col w-full h-full justify-center items-center gap-8 md:inline-block">
-          <button className="w-[70%] py-2 px-5 text-white ml-2 bg-red-600 rounded">Sign In</button>
-          <button className="w-[70%] py-2 px-5 text-white ml-2 border border-white rounded">Sign Up</button>
+        <div className="flex flex-col gap-5 md:inline-block">
+          <button className="py-2 px-5 text-white ml-2 bg-red-600 rounded">Sign In</button>
+          <button className="py-2 px-5 text-white ml-2 rounded">Sign Up</button>
         </div>
       </div>
     </div>

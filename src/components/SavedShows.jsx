b3692655cc34ba3ea4a96a373d956d81446f8a53
devxyn <johnlawrencehermo@gmail.com>
2023-12-08 15:17:36 +0800

    fix: styling in account page

--- src/components/SavedShows.jsx
+++ src/components/SavedShows.jsx
@@ -29,34 +29,31 @@ const SavedShows = () => {
 
   return (
     <>
-      <h2 className="text-white font-bold text-lg md:text-xl p-4 mt-20 md:mt-24">Movies</h2>
-      <div className="relative flex items-center ">
-        <div id={"slider"} className="flex justify-center w-full h-full gap-4 flex-row flex-wrap ">
-          {movies.map((item, id) => (
-            <div className="mb-4" key={id}>
-              <div className="w-[240px] relative p-2">
-                <img
-                  className="w-full h-auto block object-cover"
-                  src={`https://www.themoviedb.org/t/p/original/${item?.img}`}
-                  alt={item?.title}
-                />
-                <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
-                  <div className="whitespace-normal text-xs md:text-sm font-bold flex flex-col flex-wrap justify-center items-center h-full text-center p-4">
-                    <p className="mb-4">{item?.title}</p>
-                    <p
-                      onClick={() => {
-                        deleteShow(item.id);
-                      }}
-                      className="absolute text-gray-300 top-4 right-4 cursor-pointer">
-                      <IoIosClose size={32} />
-                    </p>
-                  </div>
+      <div className="mt-28 md:mt-32 flex flex-col md:flex-row md:flex-wrap md:gap-4 items-center justify-center">
+        {movies.map((item, id) => (
+          <div key={id}>
+            <div className="w-[280px] md:w-[240px] relative p-2">
+              <img
+                className="w-full h-auto block object-cover"
+                src={`https://www.themoviedb.org/t/p/original/${item?.img}`}
+                alt={item?.title}
+              />
+              <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
+                <div className="whitespace-normal text-xs md:text-sm font-bold flex flex-col flex-wrap justify-center items-center h-full text-center p-4">
+                  <p className="mb-4">{item?.title}</p>
+                  <p
+                    onClick={() => {
+                      deleteShow(item.id);
+                    }}
+                    className="absolute text-gray-300 top-4 right-4 cursor-pointer">
+                    <IoIosClose size={32} />
+                  </p>
                 </div>
-                <h2 className="w-full text-white text-center truncate text-sm font-semibold mt-2">{item.title}</h2>
               </div>
+              <h2 className="w-full text-white text-center truncate text-sm font-semibold mt-2">{item.title}</h2>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
     </>
   );
--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -5,7 +5,7 @@ const Account = () => {
   return (
     <>
       <div className="w-full text-white">
-        <img className="absolute w-full h-[200px] md:h-[300px]  object-cover" src={bg} alt="banner" />
+        <img className="absolute w-full h-[200px] md:h-[350px] lg:h-[300px] object-cover" src={bg} alt="banner" />
         <div className="bg-black/60 fixed top-0 left-0 w-full h-[550px]"></div>
         <div className="absolute top-[10%] md:top-[20%] p-8">
           <h1 className="text-3xl md:text-5xl font-bold">My Movies</h1>
